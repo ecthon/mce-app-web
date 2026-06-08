b148a70ba8ac6d87f633f52029080df9473b6cb3
ecthon <ecthon@gmail.com>
2026-06-07 21:07:12 -0300

    Add EventDetailsPage component for event detail display

---
+++ app/event-details/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import type { Metadata } from "next";
+import { ArrowRight01Icon, Clock01Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+
+
+export default function EventDetailsPage() {
+  return (
+    <main className="min-h-screen bg-zinc-50 p-6 text-zinc-900">
+        <h1 className="text-2xl font-bold mb-4">Detalhes do Evento</h1>
+    </main>
+  );
+}
