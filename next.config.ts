e468ac6b449ef425272ccd1bd9be14937fe6749b
ecthon <ecthon@gmail.com>
2026-06-13 12:42:52 -0300

    fix: add allowedDevOrigins configuration for local development

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["http://localhost:5173", 'local-origin.dev', '*.local-origin.dev', '192.168.1.*:3000'],
 };
 
 export default nextConfig;
