009c31f8ebb559ba4d24a35dea09dd50be4d6ec4
ecthon <ecthon@gmailcom>
2026-06-18 22:47:09 -0400

    style: enhance layout and spacing in EventSection for improved readability

--- components/event-section.tsx
+++ components/event-section.tsx
@@ -15,9 +15,24 @@ interface EventSectionProps {
 
 export default function EventSection({ events }: EventSectionProps) {
   return (
-    <section className="flex flex-col w-full h-full p-5">
-      <div className="flex flex-col w-full h-full">
-        <h1 className="text-[18px] font-bold mt-10">Próximo(s) evento(s)</h1>
+    <section className="flex flex-col w-full h-full p-5 space-y-6">
+      <div className="flex flex-col w-full h-auto">
+        <div className="flex flex-col gap-0">
+          <h1 className="text-md font-bold">Meus eventos</h1>
+          <p className="text-sm text-zinc-500">
+            Veja e edite seus eventos
+          </p>
+        </div>
+
+        <div className="flex flex-col w-full h-full mt-5 gap-2">
+          {events.map((event) => (
+            <EventCard key={event.id} {...event} />
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full h-auto">
+        <h1 className="text-md font-bold">Próximos eventos</h1>
 
         <div className="flex flex-col w-full h-full mt-5 gap-2">
           {events.map((event) => (
