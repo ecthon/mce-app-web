1ce7bb1e2cf902db232e830b15daf9b8b96177b3
ecthon <ecthon@gmail.com>
2026-06-20 00:43:50 -0300

    style: enhance Home component layout with header and user avatar for improved aesthetics

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { EditUser02Icon } from "@hugeicons/core-free-icons";
+
 import EventSection from "../components/event-section";
+import { Button } from "@/components/ui/button";
 
 const events = [
   {
@@ -22,6 +27,12 @@ const events = [
 export default function Home() {
   return (
     <main className="flex flex-col w-full h-screen">
+      <header className="flex items-center px-4 py-6 gap-4">
+          <figure className="flex h-14 w-14 overflow-hidden rounded-full bg-zinc-100">
+            <Image src="/default-img.png" alt="avatar" width={96} height={96} loading="eager" className="object-cover w-full h-full" />
+          </figure>
+          <h1 className="text-lg font-bold">Olá, João!</h1>
+      </header>
       <EventSection events={events} />
     </main>
   );
